25b154bd89fcc69b49a5f680d56e5dcf2d284cef
Sayantan Samanta <sssamanta789@gmail.com>
2023-05-05 01:59:16 +0530

    refactor(hooks): removed hooks and code refactoring

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,56 +1,56 @@
-import axios from "axios";
+import axios from 'axios';
 
 export interface ApiResponse<TData> {
-    data: TData;
-    status: string;
+  data: TData;
+  status: string;
 }
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_APP_API_URL_COINS,
-    headers: {
-        "X-RapidAPI-Key": import.meta.env.VITE_APP_API_KEY,
-    },
+  baseURL: import.meta.env.VITE_APP_API_URL_COINS,
+  headers: {
+    'X-RapidAPI-Key': import.meta.env.VITE_APP_API_KEY,
+  },
 });
 
 export const coinApi = async (params: string, queryParams?: object) => {
-    const res = await api.get(params, {
-        method: "GET",
-        params: queryParams,
-    });
-    return res.data;
+  const res = await api.get(params, {
+    method: 'GET',
+    params: queryParams,
+  });
+  return res.data;
 };
 
 export const newsapi = axios.create({
-    baseURL: import.meta.env.VITE_APP_API_URL_NEWS,
-    headers: {
-        "X-RapidAPI-Key": import.meta.env.VITE_APP_API_KEY,
-        "X-BingApis-SDK": "true",
-        "content-type": "application/octet-stream",
-    },
-    params: {
-        q: "Cryptocurrency",
-        freshness: "Day",
-        textFormat: "Raw",
-        safeSearch: "Off",
-        count: 10,
-    },
+  baseURL: import.meta.env.VITE_APP_API_URL_NEWS,
+  headers: {
+    'X-RapidAPI-Key': import.meta.env.VITE_APP_API_KEY,
+    'X-BingApis-SDK': 'true',
+    'content-type': 'application/octet-stream',
+  },
+  params: {
+    q: 'Cryptocurrency',
+    freshness: 'Day',
+    textFormat: 'Raw',
+    safeSearch: 'Off',
+    count: 10,
+  },
 });
 
 export const newsApi = async () => {
-    const res = await newsapi.get("");
-    return res.data;
+  const res = await newsapi.get('');
+  return res.data;
 };
 
 export const nftsapi = axios.create({
-    baseURL: import.meta.env.VITE_APP_API_URL_NFTS,
-    headers: {
-        "X-RapidAPI-Key": import.meta.env.VITE_APP_API_KEY,
-    },
+  baseURL: import.meta.env.VITE_APP_API_URL_NFTS,
+  headers: {
+    'X-RapidAPI-Key': import.meta.env.VITE_APP_API_KEY,
+  },
 });
 
 export const nftsApi = async () => {
-    const res = await nftsapi.get("/assets", {
-        params: { order_direction: "desc", limit: 20, include_orders: "false" },
-    });
-    return res.data;
+  const res = await nftsapi.get('/assets', {
+    params: { order_direction: 'desc', limit: 20, include_orders: 'false' },
+  });
+  return res.data;
 };
